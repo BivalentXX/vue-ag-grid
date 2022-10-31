6270b7f052930862807d1bf3f12965fe2fcb0250
BivalentContinew <kwapeter@gmail.com>
2022-10-31 16:08:32 -0400

    boilerplate Vue app with ag-grid table

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <ag-grid-vue
+    style="width: 500px; height: 200px"
+    class="ag-theme-alpine"
+    :columnDefs="columnDefs"
+    :rowData="rowData"
+  >
+  </ag-grid-vue>
+</template>
+
+<script>
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { AgGridVue } from "ag-grid-vue3";
+
+export default {
+  name: "App",
+  components: {
+    AgGridVue,
+  },
+  setup() {
+    return {
+      columnDefs: [
+        { headerName: "Make", field: "make" },
+        { headerName: "Model", field: "model" },
+        { headerName: "Price", field: "price" },
+      ],
+      rowData: [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxster", price: 72000 },
+      ],
+    };
+  },
+};
+</script>
