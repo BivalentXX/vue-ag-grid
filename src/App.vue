3966d684e2461205139f608ca1daba9c0fc303d9
BivalentContinew <kwapeter@gmail.com>
2022-10-31 16:13:46 -0400

    updated App.vue with dynamic data, abstrations, lifecycle events, ag-grid options, etc

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
+  <button @click="deselectRows">deselect rows</button>
   <ag-grid-vue
-    style="width: 500px; height: 200px"
     class="ag-theme-alpine"
-    :columnDefs="columnDefs"
-    :rowData="rowData"
+    style="height: 500px"
+    :columnDefs="columnDefs.value"
+    :rowData="rowData.value"
+    :defaultColDef="defaultColDef"
+    rowSelection="multiple"
+    animateRows="true"
+    @cell-clicked="cellWasClicked"
+    @grid-ready="onGridReady"
   >
   </ag-grid-vue>
 </template>
 
 <script>
-import "ag-grid-community/styles/ag-grid.css";
-import "ag-grid-community/styles/ag-theme-alpine.css";
-import { AgGridVue } from "ag-grid-vue3";
+  import { AgGridVue } from "ag-grid-vue3";  // the AG Grid Vue Component
+  import { reactive, onMounted, ref } from "vue";
+
+  import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+  import "ag-grid-community/styles/ag-theme-alpine.css"; // Optional theme CSS
 
 export default {
   name: "App",
@@ -19,18 +27,52 @@ export default {
     AgGridVue,
   },
   setup() {
+    const gridApi = ref(null); // Optional - for accessing Grid's API
+
+    // Obtain API from grid's onGridReady event
+    const onGridReady = (params) => {
+      gridApi.value = params.api;
+    };
+
+    const rowData = reactive({}); // Set rowData to Array of Objects, one Object per Row
+
+    // Each Column Definition results in one Column.
+    const columnDefs = reactive({
+      value: [
+          { field: "make" },
+          { field: "model" },
+          { field: "price" }
+      ],
+    });
+
+    // DefaultColDef sets props common to all Columns
+    const defaultColDef = {
+      sortable: true,
+      filter: true,
+      flex: 1
+    };
+
+    // Example load data from sever
+    onMounted(() => {
+      fetch("https://www.ag-grid.com/example-assets/row-data.json")
+        .then((result) => result.json())
+        .then((remoteRowData) => (rowData.value = remoteRowData));
+    });
+
     return {
-      columnDefs: [
-        { headerName: "Make", field: "make" },
-        { headerName: "Model", field: "model" },
-        { headerName: "Price", field: "price" },
-      ],
-      rowData: [
-        { make: "Toyota", model: "Celica", price: 35000 },
-        { make: "Ford", model: "Mondeo", price: 32000 },
-        { make: "Porsche", model: "Boxster", price: 72000 },
-      ],
+      onGridReady,
+      columnDefs,
+      rowData,
+      defaultColDef,
+      cellWasClicked: (event) => { // Example of consuming Grid Event
+        console.log("cell was clicked", event);
+      },
+      deselectRows: () =>{
+        gridApi.value.deselectAll()
+      }
     };
   },
 };
 </script>
+
+<style lang="scss"></style>
